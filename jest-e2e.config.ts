8b86c82857857c24511b486a410165fdb07e93c2
SocketSomeone <socket.someone@gmail.com>
2025-12-03 21:20:48 +0300

    fix: disable jest e2e problems

--- jest-e2e.config.ts
+++ jest-e2e.config.ts
@@ -1,6 +1,16 @@
-import { pathsToModuleNameMapper } from 'ts-jest';
-import { compilerOptions } from './tsconfig.json';
 import type { JestConfigWithTsJest } from 'ts-jest';
+import type { TsConfigJson } from 'type-fest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import * as fs from 'node:fs';
+
+const TSCONFIG_PATH = './tsconfig.json';
+
+if (!fs.existsSync(TSCONFIG_PATH)) {
+	throw new Error(`Cannot find tsconfig.json at path: ${TSCONFIG_PATH}`);
+}
+
+const tsConfig = JSON.parse(fs.readFileSync(TSCONFIG_PATH, 'utf8')) as TsConfigJson;
+const compilerOptions = tsConfig.compilerOptions || {};
 
 export const jestConfig: JestConfigWithTsJest = {
 	moduleFileExtensions: ['js', 'json', 'ts'],
